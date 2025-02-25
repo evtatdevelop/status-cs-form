4e4eb8e0562f4c1f687b28d69f2bf76c3bf7da38
Evgenii Tatarenko <evtatdevelop@gmail.com>
2025-02-25 15:56:18 +0700

    requestPrivsAll styles

--- src/features/statusOrder/requestPrivsAll/requestPrivsAll.js
+++ src/features/statusOrder/requestPrivsAll/requestPrivsAll.js
@@ -17,25 +17,25 @@ export const RequestPrivsAll = () => {
   return (
     <div className={styleRequestPrivsAll} >
 
-      <h3 className={styles.asked_accesses}>{dictionary.asked_access_permissions[lang]}</h3>
-      <ul className={styles.askedPermissions}>
-        <li className={styles.role}>
-          <label className={styles.tableHead}>{dictionary.role[lang]}</label>
-          <label className={styles.tableHead}>{dictionary.organizational_levels[lang]}</label>
+      <h3>{dictionary.asked_access_permissions[lang]}</h3>
+      <ul>
+        <li>
+          <label>{dictionary.role[lang]}</label>
+          <label>{dictionary.organizational_levels[lang]}</label>
 
         </li>
         {
           order?.askedPermissions
           ? Object.values(order.askedPermissions).map( (role, index) => 
-            <li key={index} className={styles.role}>
-              <label className={styles.roleName}>{`${role.asz32_name} ${role.role_cod}`}</label>
+            <li key={index}>
+              <label>{`${role.asz32_name} ${role.role_cod}`}</label>
               <ul className={styles.roleLevel}>{
                 Object.entries(role.levels).map((level, indx) => 
-                  <li key={indx} className={styles.level}>
-                    <label className={styles.levelName}>{level[0]}</label>
-                    <ul className={styles.values}>{
+                  <li key={indx}>
+                    <label>{level[0]}</label>
+                    <ul>{
                       Object.values(level[1]).map((value, i) =>
-                        <li key={i} className={styles.value}>{`${value.code_val}`}<span>{` (${value.asz06_name})`}</span></li>
+                        <li key={i}>{`${value.code_val}`}<span>{` (${value.asz06_name})`}</span></li>
                       )
                     }</ul>
                 </li>)
